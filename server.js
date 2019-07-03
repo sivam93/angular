47561987852bdeb622ef9519ae12e4c1644471fa
Sathish Kumar P <SathishKumar@teamblr.com>
2019-07-03 17:32:56 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ res.sendFile(path.join(__dirname+'/dist/login/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
